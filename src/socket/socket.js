c3dcdb7261b544c5251e8ff3a9c8d2d253433430
officialkartikey <kartikeymishra13102006@gmail.com>
2026-04-25 15:07:27 +0530

    updateprofilesocket

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,49 +1,91 @@
 const Message = require("../models/Message");
 const Notification = require("../models/Notification");
 
-let users = {}; // userId → socketId
+// 🔁 Use Map instead of object (better & safer)
+const users = new Map(); // userId → socketId
 
 module.exports = (io) => {
 
   io.on("connection", (socket) => {
 
+    console.log("🔌 User connected:", socket.id);
+
     // 🔗 Register user
     socket.on("join", (userId) => {
-      users[userId] = socket.id;
+      users.set(userId, socket.id);
+      console.log("✅ User joined:", userId);
     });
 
-    // 💬 Send message real-time
+    // 💬 Send message
     socket.on("sendMessage", async ({ sender, receiver, text }) => {
+      try {
+        const msg = await Message.create({ sender, receiver, text });
 
-      const msg = await Message.create({ sender, receiver, text });
+        // send message
+        if (users.has(receiver)) {
+          io.to(users.get(receiver)).emit("newMessage", msg);
 
-      // send to receiver
-      if (users[receiver]) {
-        io.to(users[receiver]).emit("newMessage", msg);
+          // 🔔 notification
+          io.to(users.get(receiver)).emit("notification", {
+            type: "message",
+            message: "New message received",
+            sender
+          });
+        }
+
+      } catch (err) {
+        console.error("Message error:", err);
       }
+    });
 
-      // 🔔 notification
-      if (users[receiver]) {
-        io.to(users[receiver]).emit("notification", {
-          message: "New message received"
+    // 🔥 NEW: Connection Request Notification
+    socket.on("sendConnectionRequest", async ({ senderId, receiverId }) => {
+      try {
+
+        // 🗄️ Save in DB
+        const notification = await Notification.create({
+          user: receiverId,
+          type: "connection_request",
+          message: "You have a new connection request",
+          sender: senderId
         });
+
+        // 📡 Real-time emit
+        if (users.has(receiverId)) {
+          io.to(users.get(receiverId)).emit("notification", {
+            _id: notification._id,
+            type: "connection_request",
+            message: "You have a new connection request",
+            sender: senderId
+          });
+        }
+
+      } catch (err) {
+        console.error("Connection request error:", err);
       }
     });
 
     // 🔔 Post notification
     socket.on("newPost", ({ connections, user }) => {
       connections.forEach((id) => {
-        if (users[id]) {
-          io.to(users[id]).emit("notification", {
+        if (users.has(id)) {
+          io.to(users.get(id)).emit("notification", {
+            type: "post",
             message: `${user} posted something`
           });
         }
       });
     });
 
+    // ❌ Disconnect
     socket.on("disconnect", () => {
-      for (let key in users) {
-        if (users[key] === socket.id) delete users[key];
+      console.log("❌ User disconnected:", socket.id);
+
+      for (let [key, value] of users.entries()) {
+        if (value === socket.id) {
+          users.delete(key);
+          break;
+        }
       }
     });
 
